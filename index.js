367e15b5b9b56c9e7cb7a50380cbfe29df0df0a4
Kotin Lab <tinhtunaungcu.cu@gmail.com>
2023-07-15 22:46:48 +0630

    update CORS Access-Control-Allow-Origin to *

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('port', process.env.PORT || 3001);
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://localhost:3000"
+    "*"
   );
   res.header(
     "Access-Control-Allow-Headers",
